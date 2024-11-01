d191888e045e758fac4c44b1c5563e5a37778a0d
wangyangyang <wangyangyang@joyame.com>
2024-11-01 20:22:21 +0800

    群聊的简单实现

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -51,6 +52,24 @@ func (s *Server) Handler(conn net.Conn) {
 	s.mapLock.Unlock()
 	//	and dispatch msg
 	s.BroadCast(user, "已上线")
+
+	go func() {
+		buf:=make([]byte,4096)
+		for  {
+			n,err:=conn.Read(buf)
+			if  n==0 {
+				s.BroadCast(user,"下线")
+				return
+			}
+			if err != nil && err!=io.EOF{
+				fmt.Println("Conn Read err",err)
+				return
+			}
+			msg:= string(buf[:n-1])
+			s.BroadCast(user,msg)
+
+		}
+	}()
 //	 当前handler阻塞
 	select {
 	}
