5eb0ad3fe8b9c8017729566b3d5b275fe355b2a2
PHP3p <1183162527@qq.com>
2024-11-15 12:23:36 +0800

    1-10顺序打印

--- pbfiles/syncAction.go
+++ pbfiles/syncAction.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	chStop := make(chan bool)
+	go func() {
+		i := 1
+		for {
+			select {
+			case <-ch1:
+				if i%2 != 0 {
+					fmt.Println(i)
+				}
+				i++
+				ch2 <- true
+			}
+		}
+	}()
+	go func() {
+		i := 1
+		for {
+			select {
+			case <-ch2:
+				if i%2 == 0 {
+					fmt.Println(i)
+				}
+				i++
+				if i > 10 {
+					chStop <- true
+					return
+				}
+				ch1 <- true
+			}
+		}
+	}()
+
+	// 启动时，需要给奇数协程发送一个初始信号
+	ch1 <- true
+	// 等待所有协程完成
+	<-chStop
+
+}
